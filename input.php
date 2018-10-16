f943c5f2d73bf244a64425ebdf84a5568ff898a4
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-16 13:31:09 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,28 +1,15 @@
 <?php
 
-//API Url
-$url = 'https://oudoud.herokuapp.com/input.php';
-
-//Initiate cURL.
-$ch = curl_init($url);
-
-//The JSON data.
-$jsonData = array(
-    'username' => 'MyUsername',
-    'password' => 'MyPassword'
+$data = array(
+    'userID'      => 'a7664093-502e-4d2b-bf30-25a2b26d6021',
+    'itemKind'    => 0,
+    'value'       => 1,
+    'description' => 'Boa saudaÁ„o.',
+    'itemID'      => '03e76d0a-8bab-11e0-8250-000c29b481aa'
 );
 
-//Encode the array into JSON.
-$jsonDataEncoded = json_encode($jsonData);
+$json = json_encode($data);
 
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
-
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
-
-//Execute the request
-$result = curl_exec($ch);
+$client = new Zend_Http_Client($uri);
+$client->setRawData($json, 'application/json')->request('POST');
+?>
