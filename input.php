e85c00adeda2c48a03d3b3760139365f3af6165f
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-16 13:21:02 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,2 +1,28 @@
-<?php print_r($_POST);
-?>
+<?php
+
+//API Url
+$url = 'https://oudoud.herokuapp.com/input.php';
+
+//Initiate cURL.
+$ch = curl_init($url);
+
+//The JSON data.
+$jsonData = array(
+    'username' => 'MyUsername',
+    'password' => 'MyPassword'
+);
+
+//Encode the array into JSON.
+$jsonDataEncoded = json_encode($jsonData);
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+
+//Execute the request
+$result = curl_exec($ch);
