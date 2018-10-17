8cd2486d3e8ee755ebbad4f28fbb91bc6ccf8641
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 13:43:45 +0700

    Update examole.php

--- examole.php
+++ examole.php
@@ -4,14 +4,17 @@ header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json;charset=utf-8');
-$myfile = "value.txt" ;
-$fileHandle = fopen($myfile,'r+');
-$thedata = fread($fileHandle,100);
+$data = file_get_contents("input.txt"); 
+$convert = explode("\n", $data); //create array separate by new line
+print_r($convert)
+// $myfile = "value.txt" ;
+// $fileHandle = fopen($myfile,'r+');
+// $thedata = fread($fileHandle,100);
 //   echo($thedata);
-$a = ($thedata[1]);
+// $a = ($thedata[1]);
 // $b = $a['array']
 // $a = $thedata['temp'];
-  print_r($a);
+//   print_r($a);
 // $output= '{
 // "value": [
 // {"text": "This Form OPDEV API"},
