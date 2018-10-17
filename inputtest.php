ace210ca4aa0dabb5ba1bd73294116663ac8f132
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 16:28:23 +0700

    Update inputtest.php

--- inputtest.php
+++ inputtest.php
@@ -1,22 +1,60 @@
-<?php 
-abstract class LogF{
-    static $namefile;
-    static $fp;
-    public static function setFileName($nameFile='value'){
-        self::$namefile=   $nameFile;
-        $file_log=self::$namefile.'_'.date('Ymd').'.txt';
-        self::$fp= fopen($file_log, "a+");
-    }
-    public static function Log($text){
-        $text=chr(13).date('Y-m-d H:i').'==>'.$text;
-        fwrite(self::$fp, $text);
-    }
-    public static function closeFile(){
-        fclose(self::$fp);        
-    }
+<?php
+ 
+$dataPoints = array();
+$y = 5;
+for($i = 0; $i < 10; $i++){
+	$y += rand(-1, 1) * 0.1; 
+	array_push($dataPoints, array("x" => $i, "y" => $y));
 }
-# example of use
-LogF::setFileName();
-LogF::log('primero');
-LogF::log('segundo');
-LogF::closeFile();
+ 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function() {
+ 
+var dataPoints = <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>;
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "light2",
+	title: {
+		text: "Live Internet Speed"
+	},
+	axisX:{
+		title: "Time in millisecond"
+	},
+	axisY:{
+		includeZero: false,
+		suffix: " Mbps"
+	},
+	data: [{
+		type: "line",
+		yValueFormatString: "#,##0.0#",
+		toolTipContent: "{y} Mbps",
+		dataPoints: dataPoints
+	}]
+});
+chart.render();
+ 
+var updateInterval = 1500;
+setInterval(function () { updateChart() }, updateInterval);
+ 
+var xValue = dataPoints.length;
+var yValue = dataPoints[dataPoints.length - 1].y;
+ 
+function updateChart() {
+	yValue += (Math.random() - 0.5) * 0.1;
+	dataPoints.push({ x: xValue, y: yValue });
+	xValue++;
+	chart.render();
+};
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>    
