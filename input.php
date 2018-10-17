99cc00efe6bedebf4b32c4addebaf62042bd2e25
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 10:58:45 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,9 +1,10 @@
 <?php
 require "value.txt" ;
 $myfile = "value.txt";
-$fileHandle = fopen($myfile,'w')or die("can't open file");
+$fileHandle = fopen($myfile,'r+')or die("can't open file");
 $a = ($_POST);
 $b = ($a['aa']);
-fwrite($fileHandle, $a);
+//fwrite($fileHandle, $a);
+fwrite($fileHandle, var_export($a, true));
   fclose($fileHandle);
 ?> 
