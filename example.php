48c2011e96a9f661e3915b61a22a947ebf394cb3
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 14:32:01 +0700

    Update example.php

--- example.php
+++ example.php
@@ -27,10 +27,16 @@ for ($i=41;$i<=45;$i++){
 // $b = $a['array']
 // $a = $thedata['temp'];
 //   print_r($a);
+// $output= '{
+// "value": [
+// {"temp": "'.$a.'"},
+// {"hum": "'.$b.'"}
+// ]
+// }';
 $output= '{
 "value": [
-{"temp": "'.$a.'"},
-{"hum": "'.$b.'"}
+{"temp": "a"},
+{"hum": "b"}
 ]
 }';
 print_r($output);
