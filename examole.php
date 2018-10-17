2699981f79e2cc33def2ef98da925e49d1857874
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 13:28:47 +0700

    Update examole.php

--- examole.php
+++ examole.php
@@ -8,7 +8,7 @@ $myfile = "value.txt" ;
 $fileHandle = fopen($myfile,'r');
 $thedata = fread($fileHandle,100);
 //   echo($thedata);
-$a = ($thedata['hum']);
+$a = ($thedata[hum]);
 // $b = $a['array']
 // $a = $thedata['temp'];
   print_r($a);
