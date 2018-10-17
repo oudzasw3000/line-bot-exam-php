be0410fba36e38acee754547794ef88aa4aa12c9
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 10:51:16 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,9 +1,9 @@
 <?php
 require "value.txt" ;
 $myfile = "value.txt";
-$fileHandle = fopen($myfile,'a')or die("can't open file");;
+$fileHandle = fopen($myfile,'w')or die("can't open file");
 $a = ($_POST);
 $b = ($a['aa']);
-fwrite($fileHandle, $b);
+fwrite($fileHandle, "12");
   fclose($fileHandle);
 ?> 
