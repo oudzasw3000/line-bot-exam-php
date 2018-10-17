ed13f4691796d099819b5be850b22d507be783af
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-17 10:15:21 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,8 +1,8 @@
 <?php
-$myfile = "value.txt"
-$fileHandle = fopen($myfile,'a')
+$myfile = "value.txt";
+$fileHandle = fopen($myfile,'');
 $a = ($_POST);
-$b = ($a['aa'])
-fwrite($fileHandle, $a)
-  fclose($fileHandle)
+$b = ($a['aa']);
+fwrite($fileHandle, $a);
+  fclose($fileHandle);
 ?> 
