8f21280dc5f8665a672c087e1e15fc835ce98218
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-19 10:34:52 +0700

    Update input.php

--- input.php
+++ input.php
@@ -4,14 +4,14 @@ $myfile = "value.txt";
 $fileHandle = fopen($myfile,'r+')or die("can't open file");
 $a = ($_POST["hum"]);
 $b = ($_POST["temp"]);
-print_r($a);
+//print_r($a);
 // print_r($b);
  $output= '{
 "value": [
 {"temp": "'.$b.'"},
 {"hum": "'.$a.'"}
 ]
-}'
+}';
 
 //fwrite($fileHandle, $a);
 fwrite($fileHandle, var_export($output, true));
