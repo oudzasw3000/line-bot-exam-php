c920965275247e470efdf5db401588548ed93e7d
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-19 10:06:12 +0700

    Update input.php

--- input.php
+++ input.php
@@ -3,7 +3,15 @@ require "value.txt" ;
 $myfile = "value.txt";
 $fileHandle = fopen($myfile,'r+')or die("can't open file");
 $a = ($_POST);
-$b = ($a['aa']);
+$b = ($a['hum']);
+print_r($b)
+//   $output= '{
+// "value": [
+// {"temp": "'.$a.'"},
+// {"hum": "'.$b.'"}
+// ]
+// }'
+
 //fwrite($fileHandle, $a);
 fwrite($fileHandle, var_export(json_encode($a), true));
   fclose($fileHandle);
