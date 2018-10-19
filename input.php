b14b87ad9aaf3fe827693a9d4e497e21ac110e12
oudzasw3000 <44185658+oudzasw3000@users.noreply.github.com>
2018-10-19 10:44:26 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,17 +1,17 @@
 <?php
 require "value.txt" ;
 $myfile = "value.txt";
-$fileHandle = fopen($myfile,'r+')or die("can't open file");
+$fileHandle = fopen($myfile,'r')or die("can't open file");
 $a = ($_POST["hum"]);
 $b = ($_POST["temp"]);
 //print_r($a);
 // print_r($b);
- $output= "{
-'value': [
-{'temp': '".$b."'},
-{'hum': '".$a."'}
+ $output= '{
+"value": [
+{"temp": "'.$b.'"},
+{"hum": "'.$a.'"}
 ]
-}";
+}';
 
 //fwrite($fileHandle, $a);
 fwrite($fileHandle, var_export($output, true));
